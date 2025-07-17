b16955b06f2e5061c80584a9749a74cf69f70784
mohamed-eldeghiedy <eldegedym@gmail.com>
2025-07-17 11:41:06 +0300

    Add project files.

---
+++ Assignment07_opp/Interface1.cs
@@ -0,0 +1,70 @@
+﻿
+
+namespace Assignment07_opp
+{
+    public interface IShape
+    {
+        double Area { get; }
+
+        void DisplayShapeInfo();
+    }
+
+
+    public interface ICircle : IShape
+    {
+        double Radius { get; }
+
+    }
+
+    public interface IRectangle : IShape
+    {
+        double Width { get; }
+        double Height { get; }
+    }
+
+    public class Circle : ICircle
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+    
+    
+    public double Area => Math.PI * Radius * Radius;
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape: Circle");
+            Console.WriteLine($"Radius: {Radius}");
+            Console.WriteLine($"Area: {Area:F2}");
+            Console.WriteLine();
+        }
+    }
+
+
+
+    public class Rectangle : IRectangle
+    {
+        public double Width { get; private set; }
+        public double Height { get; private set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public double Area => Width * Height;
+
+        public void DisplayShapeInfo()
+        {
+            Console.WriteLine("Shape: Rectangle");
+            Console.WriteLine($"Width: {Width}");
+            Console.WriteLine($"Height: {Height}");
+            Console.WriteLine($"Area: {Area:F2}");
+            Console.WriteLine();
+        }
+    }
+}
---
+++ Assignment07_opp/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Assignment07_opp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+
+
+
+            // part 02
+            #region q1: Define an interface named IShape with a property Area and a method DisplayShapeInfo. Create two interfaces, ICircle and IRectangle, that inherit from IShape. Implement these interfaces in classes Circle and Rectangle. Test your implementation by creating instances of both classes and displaying their shape information.
+
+
+            //ICircle circle = new Circle(3.0);
+            //IRectangle rectangle = new Rectangle(3.0, 5.0);
+
+            //circle.DisplayShapeInfo();
+            //rectangle.DisplayShapeInfo();
+            #endregion
+
+
+            #region q2: 
+
+
+            #endregion
+    }
+}
